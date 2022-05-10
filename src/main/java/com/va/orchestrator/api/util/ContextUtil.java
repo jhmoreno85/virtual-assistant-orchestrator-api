4d873958c3c8dc32219f47942651b307d6563a08
Jorge Huerta <huerta.jorge@gmail.com>
2022-05-09 23:18:58 -0500

    Improvements

--- src/main/java/com/va/orchestrator/api/util/ContextUtil.java
+++ src/main/java/com/va/orchestrator/api/util/ContextUtil.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.va.orchestrator.api.exception.BadRequestException;
 import com.va.orchestrator.api.exception.ErrorCode;
-import com.va.orchestrator.api.model.sendmessage.Channel;
 import com.va.orchestrator.api.model.sendmessage.CommunicationContext;
 import com.va.orchestrator.api.model.sendmessage.Context;
 import com.va.orchestrator.api.model.sendmessage.SendMessageRequest;
@@ -66,11 +65,12 @@ public class ContextUtil {
       }
     } else if (object instanceof List && targetPath[currIndex].matches(REGEX_ARRAY_BRACKETS)) {
       List<Object> list = (List<Object>) object;
-      int elementIndex = Integer.parseInt(targetPath[currIndex].substring(1, targetPath[currIndex].length() - 1));
+      int elementIndex =
+          Integer.parseInt(targetPath[currIndex].substring(1, targetPath[currIndex].length() - 1));
       if (elementIndex < list.size()) {
         return currIndex == targetPath.length - 1
-                ? list.get(elementIndex)
-                : getTargetObjectFromMap(list.get(elementIndex), targetPath, currIndex + 1);
+            ? list.get(elementIndex)
+            : getTargetObjectFromMap(list.get(elementIndex), targetPath, currIndex + 1);
       }
     }
     return null;
@@ -79,35 +79,29 @@ public class ContextUtil {
   public static void populateCommunicationContext(
       Map<String, Object> context, SendMessageRequest sendMessageRequest)
       throws BadRequestException {
-    String channelType =
-        Optional.of(sendMessageRequest)
+    String[] channelTypeAndSubtype =
+        Optional.ofNullable(sendMessageRequest)
             .map(SendMessageRequest::getContext)
             .map(Context::getCommunicationContext)
             .map(CommunicationContext::getChannel)
-            .map(Channel::getChannelType)
-            .map(String::trim)
-            .filter(currChannelType -> !currChannelType.isEmpty())
+            .filter(ch -> null != ch.getChannelType() && null != ch.getChannelSubtype())
+            .map(
+                ch -> {
+                  ch.setChannelType(ch.getChannelType().trim());
+                  ch.setChannelSubtype(ch.getChannelSubtype().trim());
+                  return ch;
+                })
+            .filter(ch -> !ch.getChannelType().isEmpty() && !ch.getChannelSubtype().isEmpty())
+            .map(ch -> new String[] {ch.getChannelType(), ch.getChannelSubtype()})
             .orElseThrow(
                 () ->
                     new BadRequestException(
-                        "Communication Context - Channel Type is null or empty",
+                        "Communication Context - Channel Type/Subtype is null or empty",
                         new Exception(),
                         ErrorCode.INVALID_PAYLOAD));
-    String channelSubtype =
-        Optional.of(sendMessageRequest)
-            .map(SendMessageRequest::getContext)
-            .map(Context::getCommunicationContext)
-            .map(CommunicationContext::getChannel)
-            .map(Channel::getChannelSubtype)
-            .map(String::trim)
-            .filter(currChannelSubtype -> !currChannelSubtype.isEmpty())
-            .orElseThrow(
-                () ->
-                    new BadRequestException(
-                        "Communication Context - Channel Subtype is null or empty",
-                        new Exception(),
-                        ErrorCode.INVALID_PAYLOAD));
-    context.put("communication_context", createCommunicationContext(channelType, channelSubtype));
+    context.put(
+        "communication_context",
+        createCommunicationContext(channelTypeAndSubtype[0], channelTypeAndSubtype[1]));
   }
 
   private static Map<String, Object> createCommunicationContext(
